9089937e4bcff8e9beae4dc3eb60e85acf160d8c
Teo <teo.ionescu@gmail.com>
2021-01-30 19:53:27 +0200

    nicer texts in splash and help; added sound support with someplaceholder sounds

---
+++ ScatteredFlock/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource atractSource;
+    public AudioSource scareSource;
+
+    public AudioSource ambientMusicSource;
+    public AudioSource startMusicSource;
+    public AudioSource endMusicSource;
+
+    public AudioSource effectsSource;
+
+    public AudioClip hitEffectClip;
+    public AudioClip enterEffectClip;
+
+    public void StopAll()
+    {
+        if (atractSource != null && atractSource.isPlaying)
+        {
+            atractSource.Stop();
+        }
+        if (scareSource != null && scareSource.isPlaying)
+        {
+            scareSource.Stop();
+        }
+
+        if (ambientMusicSource != null && ambientMusicSource.isPlaying)
+        {
+            ambientMusicSource.Stop();
+        }
+        if (endMusicSource != null && endMusicSource.isPlaying)
+        {
+            endMusicSource.Stop();
+        }
+        if (startMusicSource != null && startMusicSource.isPlaying)
+        {
+            startMusicSource.Stop();
+        }
+
+        if (effectsSource != null && effectsSource.isPlaying)
+        {
+            effectsSource.Stop();
+        }
+    }
+
+    public void StopSounds()
+    {
+        if (atractSource != null && atractSource.isPlaying)
+        {
+            atractSource.Stop();
+        }
+        if (scareSource != null && scareSource.isPlaying)
+        {
+            scareSource.Stop();
+        }
+
+        if (effectsSource != null && effectsSource.isPlaying)
+        {
+            effectsSource.Stop();
+        }
+    }
+
+    public void StopMusic()
+    {
+        if (ambientMusicSource != null && ambientMusicSource.isPlaying)
+        {
+            ambientMusicSource.Stop();
+        }
+        if (endMusicSource != null && endMusicSource.isPlaying)
+        {
+            endMusicSource.Stop();
+        }
+        if (startMusicSource != null && startMusicSource.isPlaying)
+        {
+            startMusicSource.Stop();
+        }
+    }
+
+    public void PlayAttractSound()
+    {
+        if (atractSource != null && !atractSource.isPlaying)
+        {
+            atractSource.Play();
+        }
+    }
+    public void StopAttractSound()
+    {
+        if (atractSource != null && atractSource.isPlaying)
+        {
+            atractSource.Stop();
+        }
+    }
+
+    public void PlayScareSound()
+    {
+        if (scareSource != null && !scareSource.isPlaying)
+        {
+            scareSource.Play();
+        }
+    }
+    public void StopScareSound()
+    {
+        if (scareSource != null && scareSource.isPlaying)
+        {
+            scareSource.Stop();
+        }
+    }
+
+    public void PlayAmbientMusic()
+    {
+        if (ambientMusicSource != null && !ambientMusicSource.isPlaying)
+        {
+            ambientMusicSource.Play();
+        }
+    }
+    public void StopAmbientMusic()
+    {
+        if (ambientMusicSource != null && ambientMusicSource.isPlaying)
+        {
+            ambientMusicSource.Stop();
+        }
+    }
+
+    public void PlayStartMusic()
+    {
+        if (startMusicSource != null && !startMusicSource.isPlaying)
+        {
+            startMusicSource.Play();
+        }
+    }
+    public void StopStartMusic()
+    {
+        if (startMusicSource != null && startMusicSource.isPlaying)
+        {
+            startMusicSource.Stop();
+        }
+    }
+
+    public void PlayEndMusic()
+    {
+        if (endMusicSource != null && !endMusicSource.isPlaying)
+        {
+            endMusicSource.Play();
+        }
+    }
+    public void StopEndMusic()
+    {
+        if (endMusicSource != null && endMusicSource.isPlaying)
+        {
+            endMusicSource.Stop();
+        }
+    }
+
+    public void PlayHitEffect()
+    {
+        if (effectsSource != null && hitEffectClip != null)
+        {
+            effectsSource.PlayOneShot(hitEffectClip);
+        }
+    }
+
+    public void PlayEnterEffect()
+    {
+        if (effectsSource != null && enterEffectClip != null)
+        {
+            effectsSource.PlayOneShot(enterEffectClip);
+        }
+    }
+}
